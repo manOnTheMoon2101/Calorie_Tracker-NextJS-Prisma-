519914e5acc660b47b309a86e801c63dede9373b
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2023-11-01 19:41:30 +0200

    update

--- app/graphs/page.jsx
+++ app/graphs/page.jsx
@@ -52,7 +52,7 @@ ChartJS.register(
 
 
   async function getData() {
-    const res = await fetch("http://localhost:3000/api/posts", { cache: 'no-store' });
+    const res = await fetch("http://localhost:3000/api/posts/getOctober", { cache: 'no-store' });
   
     if(!res.ok) {
         throw new Error("Failed to fetch data")
@@ -61,6 +61,15 @@ ChartJS.register(
     return res.json();
   }
 
+  async function getNo() {
+    const res = await fetch("http://localhost:3000/api/posts/getNovember", { cache: 'no-store' });
+  
+    if(!res.ok) {
+        throw new Error("Failed to fetch data")
+    }
+  
+    return res.json();
+  }
 
 export default async function Graphs() {
 
@@ -80,6 +89,11 @@ export default async function Graphs() {
   },[]);
 
   const posts = await getData();
+  const novemB = await getNo();
+
+
+  
+  
   return (
     <>
     <NavBar/>
@@ -130,6 +144,68 @@ export default async function Graphs() {
             
             
             data: posts.map((x) => x.weight),
+            
+            backgroundColor:'rgb(233, 184, 36)',
+hoverBackgroundColor:'rgb(255, 99, 132)',
+borderWidth:4,
+fill:true,
+            borderColor:'rgb(216, 63, 49)',
+            pointBorderColor: '#111',
+            pointBackgroundColor: 'rgb(238, 147, 34)',
+            pointBorderWidth: 2,
+          }
+          
+        ]}}
+          
+      />
+    
+    
+
+
+
+
+
+    <Line
+    ref={ref}
+
+   width={600}
+    height={400}
+    options={{
+      
+      responsive:false,
+     
+      
+      scales:{
+        
+        x :{
+          ticks:{color:'white'}
+        }
+      },
+      
+      plugins:{
+      
+       
+      
+      title:{
+        display:true,
+        text:"November 2023",
+        color:'white'
+      },
+      
+
+    }}}
+        data={{
+          labels:novemB.map((x) => x.date),
+          
+          datasets:[
+          {
+            
+            
+            
+            label:'Weight',
+            
+            
+            data: novemB.map((x) => x.weight),
             
             backgroundColor:'rgb(233, 184, 36)',
 hoverBackgroundColor:'rgb(255, 99, 132)',
